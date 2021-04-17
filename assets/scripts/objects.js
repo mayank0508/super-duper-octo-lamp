8a9d5d7e7005c88715a5930efbb51b59ec44d18f
mayank0508 <thakurmayank0508@gmail.com>
2021-04-17 13:19:42 +0530

    new

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,6 +1,25 @@
+const movielist = document.getElementById('movie-list');
+movielist.style['background-color'] ='red';
+movielist.style.display = 'block';
+
+// const advi = 'mayank1'
+const key = 'mayank'
+
 const person = {
-  name: 'John',
+  'first-name': 'John',
   sex: 'M',
+  [key]: 'mm',
   hobbies: ['football', 'cricket'],
-  greet
+  greet: function() {
+    alert('Hello')
+  }
 };
+
+person.isAdmin = true;
+
+delete person.sex;
+// person.greet();
+
+console.log(person);
+console.log(person['mayank']);
+console.log(person["first-name"]);
