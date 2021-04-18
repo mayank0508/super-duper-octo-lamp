237a71a7a3ad32194e6072862b09df78577d1080
mayank0508 <thakurmayank0508@gmail.com>
2021-04-18 09:47:38 +0530

    button added

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,16 +1,50 @@
-searchBox = document.getElementById('search-btn');
-addMovieButton = document.getElementById('add-movie-btn');
+const addMovieButton = document.getElementById('add-movie-btn');
+const searchBox = document.getElementById('search-btn');
+
+const movies = [];
+
+const renderMovie = () => {
+   const movieList = document.getElementById('movie-list');
+
+   if (movies.length === 0) {
+     movieList.classList.remove('visible');
+     return;
+   } else {
+     movieList.classList.add('visible');
+   } 
+   movieList.innerHTML = '' // it is used to remove all the existing files is the code
+
+   movies.forEach((movie) => {
+       const movieEL = document.createElement('li');
+       movieEL.textContent = movies.info.title;
+       movieList.append(movieEL);
+   });
+};
 
 const addMovieHandler = () => {
   const title = document.getElementById('title').value;
   const extraName = document.getElementById('extra-name').value;
   const extraValue = document.getElementById('extra-value').value;
-}
 
-if (
-  title.trim() === '' ||
-  extraName.trim() === '' ||
-  extraValue.trim()
-) {
-  return;
-}
+  if (
+    title.trim() === '' ||
+    extraName.trim() === '' ||
+    extraValue.trim() === ''
+  ) {
+    return;
+  }
+  
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue
+    },
+    id: Math.random(),
+  };
+  
+  movies.push(newMovie);
+  renderMovie();
+};
+
+addMovieButton.addEventListener('click', addMovieHandler);
+
